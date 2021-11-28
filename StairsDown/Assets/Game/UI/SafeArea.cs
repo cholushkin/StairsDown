1f49877dbc1b97149d14d63afb38b5e245adf3b1
Ink <i@am.com>
2021-11-27 20:35:24 -0500

    + safe areas in UI

---
+++ StairsDown/Assets/Game/UI/SafeArea.cs
@@ -0,0 +1,54 @@
+using GameLib.Log;
+using UnityEngine;
+
+// https://www.behance.net/gallery/109643193/Updating-your-GUI-for-iPhone-X-and-Notched-Devices
+public class SafeArea : MonoBehaviour
+{
+    RectTransform Panel;
+    Rect LastSafeArea = new Rect(0, 0, 0, 0);
+
+    public LogChecker LogChecker;
+
+    void Awake()
+    {
+        Panel = GetComponent<RectTransform>();
+        Refresh();
+    }
+
+    void Update()
+    {
+        Refresh();
+    }
+
+    void Refresh()
+    {
+        Rect safeArea = GetSafeArea();
+
+        if (safeArea != LastSafeArea)
+            ApplySafeArea(safeArea);
+    }
+
+    Rect GetSafeArea()
+    {
+        return Screen.safeArea;
+    }
+
+    void ApplySafeArea(Rect r)
+    {
+        LastSafeArea = r;
+
+        // Convert safe area rectangle from absolute pixels to normalised anchor coordinates
+        Vector2 anchorMin = r.position;
+        Vector2 anchorMax = r.position + r.size;
+        anchorMin.x /= Screen.width;
+        anchorMin.y /= Screen.height;
+        anchorMax.x /= Screen.width;
+        anchorMax.y /= Screen.height;
+        Panel.anchorMin = anchorMin;
+        Panel.anchorMax = anchorMax;
+
+        if (LogChecker.Verbose())
+            Debug.LogFormat("New safe area applied to {0}: x={1}, y={2}, w={3}, h={4} on full extents w={5}, h={6}",
+                name, r.x, r.y, r.width, r.height, Screen.width, Screen.height);
+    }
+}
